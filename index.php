7e81803a003c40c5af9d2aa33edd99e9e414f708
Xenophylius <sirxofficial@gmail.com>
2024-01-22 11:33:16 +0100

    Test de pseudo

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 
     <h1>Le titre de Christophe</h1>
     <p>La branche de Christophe</p>
+    <p>TEST USERNAME</p>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js" integrity="sha512-GWzVrcGlo0TxTRvz9ttioyYJ+Wwk9Ck0G81D+eO63BaqHaJ3YZX9wuqjwgfcV/MrB2PhaVX9DkYVhbFpStnqpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
