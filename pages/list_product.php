094e7202c9e974d095f738292afe5ba120403fb5
Xenophylius <sirxofficial@gmail.com>
2024-01-24 16:46:08 +0100

    Ajout like/unlike, modify product, commentary, affichage commentaire

--- pages/list_product.php
+++ pages/list_product.php
@@ -38,7 +38,29 @@ if (empty($_SESSION['id']) &&
                             $like = $counterLike->fetch();
                         ?>
 
-                    <a href="../process/product/like_product.php?id_product=<?= $productsAll[$key]['id'] ?>" class="btn btn-success"><i class="fa-solid fa-circle-up fa-2xl"></i><h5 class="d-inline font-weight-bold px-2"><?= $like[0] ?></h5></a>
+                    <a href="../process/product/like_product.php?id_product=<?= $productsAll[$key]['id'] ?>" class="btn btn-success"><i class="fa-solid fa-circle-up fa-2xl"></i><h5 class="d-inline font-weight-bold px-2"><?= $like[0] ?></h5></a><br>
+                    <form action="../process/product/add_commentary.php?id_product=<?= $productsAll[$key]['id'] ?>" method="post" class="d-flex align-items-center justify-content-center">
+                        <textarea name="commentary" id="commentary" rows="5" placeholder="Ajoutez votre commentaire..."></textarea>
+                        <button type="submit" class="btn btn-success">Envoyer</button>
+                    </form>
+
+                        <?php 
+                            // Requete pour récupérer les commentaires
+                            $listCommentary = $db->prepare("SELECT * FROM commentary INNER JOIN users ON commentary.id_user = users.id WHERE id_product=?");
+                            $listCommentary->execute([$id_product]);
+                            $commentary = $listCommentary->fetchAll();
+
+                            // Boucle pour fetchAll les commentaires
+                            foreach ($commentary as $key => $value) { ?>
+                                <section class="w-100">
+                                    <h5>Commentaire de : <?= $commentary[$key]['pseudo'] ?> du <i><?= $commentary[$key]['created_at'] ?></i></h5>
+                                    <p>Le commentaire : <?= $commentary[$key]['commentary'] ?></p>
+                                </section>
+
+                            <?php  } ?>
+                        
+
+                        
                 </section>
           <?php  } ?>
 </main>
---
+++ process/product/add_commentary.php
@@ -0,0 +1,31 @@
+<?php 
+session_start();
+
+// Si l'utilisateur n'est pas connecté, redirigé vers la page de connexion/inscription
+if (empty($_SESSION['id']) &&
+    empty($_SESSION['pseudo']) &&
+    empty($_SESSION['mail'])) {
+        header('Location: ../index.php?error=Inscrivez vous ou connectez vous pour accéder à cette page.');
+        die;
+}
+
+
+
+// Vérification que le commentaire n'est pas vide
+if (empty($_POST['commentary'])) {
+    header('Location: ../../pages/list_product.php?error=Votre commentaire n\'est pas valide.');
+    die;
+}
+
+// Connexion à la BDD et injection du commentaire
+require_once '../connexion.php';
+$addCommentary = $db->prepare("INSERT INTO commentary (id_user, id_product, created_at, commentary) VALUES (?, ?, ?, ?)");
+$addCommentary->execute([
+    $_SESSION['id'],
+    $_GET['id_product'],
+    date("Y-m-d H:i:s"),
+    $_POST['commentary']
+]);
+
+header('Location: ../../pages/list_product.php?success=Merci pour votre commentaire');
+die;
