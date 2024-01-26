51349dda01e9ee1af95bb26e3187a60b0fa21ba1
Xenophylius <sirxofficial@gmail.com>
2024-01-26 09:18:48 +0100

    Test pour modal avec Ajax et js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 console.log("coucou");
 
 console.log(element.dataset.id);
+
+// Changer la couleur 
+
+function changeColor() {
+    let bgMain = document.querySelector(':root')
+    bgMain.style.setProperty('--main-color', '#2C439B')
+    bgMain.style.setProperty('--second-color', '#33C7F7')
+}
---
+++ pages/liste_utilisateurs.php
@@ -0,0 +1,35 @@
+<?php 
+session_start();
+
+// Si l'utilisateur n'est pas connecté, redirigé vers la page de connexion/inscription
+if (empty($_SESSION['id']) &&
+    empty($_SESSION['pseudo']) &&
+    empty($_SESSION['mail'])) {
+        header('Location: ../index.php?error=Inscrivez vous ou connectez vous pour accéder à cette page.');
+        die;
+}
+
+require_once '../partials/header.php'; 
+include_once '../partials/message.php';
+
+require_once '../process/connexion.php';
+    $listUsers = $db->prepare("SELECT * FROM users");
+    $listUsers->execute();
+    $users = $listUsers->fetchAll();
+?>
+
+<main>
+    <h1 class="textColor text-center">Liste des utilisateurs</h1>
+        <?php 
+            foreach ($users as $key => $value) { ?>
+                <section class="border-bottom">
+                    <img src="../upload/photoProfil/<?= $users[$key]['image']?>" alt="Photo de profil" style="height: 100px;" class="my-3 mx-5">
+                    <h3 class="textColor d-inline"><?= ucfirst($users[$key]['pseudo']) ?></h3>
+                </section>
+          <?php } ?>
+    
+</main>
+
+<?php
+    require_once '../partials/footer.php';
+?>
